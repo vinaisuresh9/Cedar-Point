65c8c2230ad62575de2cdc84921af39bd06cd442
Vinai Suresh <vinaisuresh9@gmail.com>
2012-10-27 11:28:06 -0400

    Adding rides to list

--- My First App/gen/com/example/my/first/app/R.java
+++ My First App/gen/com/example/my/first/app/R.java
@@ -8,9 +8,6 @@
 package com.example.my.first.app;
 
 public final class R {
-    public static final class array {
-        public static final int Rides=0x7f050000;
-    }
     public static final class attr {
     }
     public static final class drawable {
@@ -18,22 +15,28 @@ public final class R {
         public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int b_streak=0x7f080001;
-        public static final int blue_streak=0x7f080000;
-        public static final int menu_settings=0x7f080002;
+        public static final int blue_streak=0x7f070000;
+        public static final int blue_streak_button=0x7f070001;
+        public static final int calypso=0x7f070002;
+        public static final int calypso_button=0x7f070003;
+        public static final int cedar_mine=0x7f070004;
+        public static final int cedar_mine_button=0x7f070005;
+        public static final int menu_settings=0x7f070006;
     }
     public static final class layout {
         public static final int activity_display_message=0x7f030000;
         public static final int activity_main=0x7f030001;
     }
     public static final class menu {
-        public static final int activity_display_message=0x7f070000;
-        public static final int activity_main=0x7f070001;
+        public static final int activity_display_message=0x7f060000;
+        public static final int activity_main=0x7f060001;
     }
     public static final class string {
         public static final int app_name=0x7f040000;
         public static final int blue_streak=0x7f040009;
         public static final int button_send=0x7f040004;
+        public static final int calypso=0x7f04000b;
+        public static final int cedar_creek_mine=0x7f04000a;
         public static final int disabled=0x7f040008;
         public static final int edit_message=0x7f040003;
         public static final int enabled=0x7f040007;
@@ -43,6 +46,6 @@ public final class R {
         public static final int title_activity_main=0x7f040002;
     }
     public static final class style {
-        public static final int AppTheme=0x7f060000;
+        public static final int AppTheme=0x7f050000;
     }
 }
