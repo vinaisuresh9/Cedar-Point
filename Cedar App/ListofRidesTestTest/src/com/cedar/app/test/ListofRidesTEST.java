3cad66c1d369083d064bab8429302eaa620fbfe5
Vinai Suresh <vinaisuresh9@gmail.com>
2012-11-12 13:04:41 -0500

    Test Cases

---
+++ Cedar App/ListofRidesTestTest/src/com/cedar/app/test/ListofRidesTEST.java
@@ -0,0 +1,19 @@
+package com.cedar.app.test;
+
+import com.cedar.app.ListofRides;
+
+import android.test.ActivityInstrumentationTestCase2;
+
+public class ListofRidesTEST extends
+		ActivityInstrumentationTestCase2<ListofRides> {
+
+
+	public ListofRidesTEST(String name) {
+		super(name);
+	}
+
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+}
