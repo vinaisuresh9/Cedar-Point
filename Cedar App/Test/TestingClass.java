0ffe342a705def0725a10219c4e8607ddd2f6efe
Vinai Suresh <vinaisuresh9@gmail.com>
2012-11-12 11:06:44 -0500

    Test cases

---
+++ Cedar App/Test/TestingClass.java
@@ -0,0 +1,26 @@
+package com.cedar.app;
+
+import static org.junit.Assert.*;
+import java.lang.*;
+
+import org.junit.Test;
+
+
+public class TestingClass {
+
+	@Test
+	public void testAddtoList() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testViewMap() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testOnClick() {
+		fail("Not yet implemented");
+	}
+
+}
