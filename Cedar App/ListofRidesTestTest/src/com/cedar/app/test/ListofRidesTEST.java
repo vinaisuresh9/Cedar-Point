bb897490d9b3b054dd3755b8e796c293656d3a5b
Vinai Suresh <vinaisuresh9@gmail.com>
2012-11-12 13:11:36 -0500

    adding tests

--- Cedar App/ListofRidesTestTest/src/com/cedar/app/test/ListofRidesTEST.java
+++ Cedar App/ListofRidesTestTest/src/com/cedar/app/test/ListofRidesTEST.java
@@ -1,19 +1,29 @@
 package com.cedar.app.test;
 
+import java.util.*;
+
 import com.cedar.app.ListofRides;
 
 import android.test.ActivityInstrumentationTestCase2;
 
 public class ListofRidesTEST extends
 		ActivityInstrumentationTestCase2<ListofRides> {
-
+	public List<String> ridesList;
 
 	public ListofRidesTEST(String name) {
-		super(name);
+		super(ListofRides.class);
 	}
 
 	protected void setUp() throws Exception {
 		super.setUp();
+		ListofRides LOR = getActivity();
+		ridesList = new LinkedList<String>();
+	}
+	
+	public void testAddtoList()
+	{
+		
+		
 	}
 
 }
