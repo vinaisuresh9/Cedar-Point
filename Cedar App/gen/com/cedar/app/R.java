e5ea7916d98e7d886546b03f29c4e482e4daa0f7
Vinai Suresh <vinaisuresh9@gmail.com>
2012-12-06 22:45:07 -0500

    new logo

--- Cedar App/gen/com/cedar/app/R.java
+++ Cedar App/gen/com/cedar/app/R.java
@@ -20,6 +20,7 @@ public final class R {
         public static final int cedarpointmap=0x7f020000;
         public static final int ic_action_search=0x7f020001;
         public static final int ic_launcher=0x7f020002;
+        public static final int logo=0x7f020003;
     }
     public static final class id {
         public static final int blue_streak=0x7f080003;
